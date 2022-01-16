0239b5a4ad27580b4549b03a755461188fb90a83
tdmitryny <tdmitryny@gmail.com>
2022-01-16 15:31:44 -0500

    Created Two Boxes

---
+++ src/components/FirstBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function FirstBox() {
+    return (
+        <div className="FirstBox">
+            First Box
+        </div>
+    )
+}
+
+export default FirstBox
---
+++ src/components/SecondBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function SecondBox() {
+    return (
+        <div className="SecondBox">
+            Second box
+        </div>
+    )
+}
+
+export default SecondBox
